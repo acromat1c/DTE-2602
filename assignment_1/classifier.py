243743dea28ba5f6fe6748922b75b319e1b47549
acromat1c <i-reseland@outlook.com>
2024-08-27 00:53:18 +0200

    Add files via upload

--- assignment_1/classifier.py
+++ assignment_1/classifier.py
@@ -14,17 +14,9 @@ def getSkynet():
 
 #takes variables X and classifies the animal
 def classify_animal(*X):
-    #formats input so that its correct in case its wrong
+    #formats input so that its correct in case its wrong. lets you input as: 1,0,1...   [1,0,1...]   [[1,0,1...],[1,0,1...],[1,0,1...]...]
     if type(X[0]) != list: X = [X]
     elif type(X[0][0]) != list: ...
     elif type(X[0][0][0]) != list: X = X[0]
     #runs input through ai and returns output
     return getSkynet().predict(X)
-
-
-# lets you manualy enter data if you run the file
-if __name__ == "__main__":
-    inp= []
-    for i in ["hair", "feathers", "eggs", "milk", "airborne", "aquatic", "predator", "toothed", "backbone", "breathes", "venomous", "fins", "legs", "tail", "domestic", "catsize"]:
-        print(i,": ",sep="",end="");inp.append(input())
-    classify_animal(inp)
