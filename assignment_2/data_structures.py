7e58fbcd805fbd200a7f39f51530a3635f9fbf01
acromat1c <i-reseland@outlook.com>
2024-08-28 21:48:32 +0200

    Add files via upload

--- assignment_2/data_structures.py
+++ assignment_2/data_structures.py
@@ -4,8 +4,8 @@ from __future__ import annotations  # Needed for typing Node class
 #import warnings
 from typing import Any
 
-#import binarytree
-#import heapdict
+import binarytree
+import heapdict
 
 
 # Node class (do not change)
@@ -42,86 +42,55 @@ class Stack:
 
 class Queue:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = Node()
+        self.tail = self.head
 
-    def enqueue(self, data: Any) -> None:
-        if self.tail == None:
-            self.tail = [Node(data=data)]
-            self.head = [Node(data=data,next=self.tail)]
+    def enqueue(self, data: Any, node = None) -> None:
+        if node == None:self.enqueue(data,node=self.tail);return
+        if node.data is None:
+            node.data = data
+            node.next = Node()
+            self.tail = node
+            print(node.data)
             return
-        self.tail[0].next = [Node(data=data)]
-        self.tail = self.tail[0].next
+        self.enqueue(data,node=node.next)
 
     def peek(self) -> Node | None:
         return self.head.data
 
     def dequeue(self) -> Node:
-        if self.head == None:
-            raise IndexError
-            return
-        self.head = self.head[0].next
+        return self.head.data
 
 
 class EmergencyRoomQueue:
     def __init__(self):
-        """Initialize emergency room queue, use heapdict as property 'queue'"""
-        pass
+        self.hd = heapdict.heapdict()
 
     def add_patient_with_priority(self, patient_name: str, priority: int) -> None:
-        """Add patient name and priority to queue
-
-        # Arguments:
-        patient_name:   String with patient name
-        priority:       Integer. Higher priority corresponds to lower-value number.
-        """
-        pass
+        self.hd[str(patient_name)] = int(priority)
 
     def update_patient_priority(self, patient_name: str, new_priority: int) -> None:
-        """Update the priority of a patient which is already in the queue
-
-        # Arguments:
-        patient_name:   String, name of patient in queue
-        new_priority:   Integer, updated priority for patient
-
-        """
-        pass
+        self.hd[str(patient_name)] = int(new_priority)
 
     def get_next_patient(self) -> str:
-        """Remove highest-priority patient from queue and return patient name
-
-        # Returns:
-        patient_name    String, name of patient with highest priority
-        """
-        pass
+        print(self.hd.peekitem())
 
 
 class BinarySearchTree:
     def __init__(self, root: binarytree.Node | None = None):
-        """Initialize binary search tree
+        self.root = binarytree.Node(root)
 
-        # Inputs:
-        root:    (optional) An instance of binarytree.Node which is the root of the tree
-
-        # Notes:
-        If a root is supplied, validate that the tree meets the requirements
-        of a binary search tree (see property binarytree.Node.is_bst ). If not, raise
-        ValueError.
-        """
-        pass
-
-    def insert(self, value: float | int) -> None:
-        """Insert a new node into the tree (binarytree.Node object)
-
-        # Inputs:
-        value:    Value of new node
-
-        # Notes:
-        The method should issue a warning if the value already exists in the tree.
-        See https://docs.python.org/3/library/warnings.html#warnings.warn
-        In the case of duplicate values, leave the tree unchanged.
-        """
-        pass
+    def insert(self, value: float | int, node = None) -> None:
+        if node == None:self.insert(value,node=self.root);return
+        if node.value >= value:
+            if node.right is None:
+                node.right = binarytree.Node(value)
+                return
+            self.insert(value,node=node.right);return
+        if node.left is None:
+            node.left = binarytree.Node(value)
+            return
+        self.insert(value,node=node.left);return
 
     def __str__(self) -> str | None:
         """Return string representation of tree (helper function for debugging)"""
@@ -139,6 +108,22 @@ class BinarySearchTree:
 
 
 if __name__ == "__main__":
-    n = Stack()
-    for i in [1,2,3,4,5,6,7,8,9,10]:    
-        n.push(i)
+    #n = Queue()
+    #for i in [1,2,3,4,5,6,7,8,9,10]:    
+        #n.enqueue(i)
+    #print(n.tail.data)
+    # c = EmergencyRoomQueue()
+    # c.add_patient_with_priority("jack",5)
+    # c.add_patient_with_priority("Hack",5)
+    # c.add_patient_with_priority("Kack",2)
+    # c.add_patient_with_priority("Lack",9)
+    # c.update_patient_priority("lack", 2)
+    # c.get_next_patient()
+    h = BinarySearchTree(7)
+    h.insert(5)
+    h.insert(9)
+    h.insert(1)
+    h.insert(2)
+    h.insert(8)
+    h.insert(4)
+    print(h)
